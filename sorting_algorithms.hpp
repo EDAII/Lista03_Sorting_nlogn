47797bf81e7cb03010cc57cf273a09345db724c0
Lucas Hiroshi Horinouchi <lucas.hh@hotmail.com>
2018-04-09 23:12:22 -0300

    Update sorting_algorithms.hpp

--- sorting_algorithms.hpp
+++ sorting_algorithms.hpp
@@ -1,5 +1,59 @@
 #include <vector>
 #include <algorithm>
+#include <chrono>
+
+using namespace std;
+
+int getdigit(int i, int d)
+{
+    while (d > 1) {
+        d--;
+        i/=10;
+    }
+
+    return i%10;
+}
+
+int getnumofdigit(int i)
+{
+    int d = 0;
+    while(i) {
+        i/=10;
+        d++;
+    }
+    return d;
+}
+
+void countingsort(vector<int> &vi, int d)
+{
+    vector<int> out(vi.size(),0);
+    vector<int> c(10,0); // 0 to 9
+
+    for (int i=0;i<vi.size();i++) {
+        c[getdigit(vi[i],d)]++;
+    }
+    // c[i] now contains the number of elements equal to i.
+
+    for (int i=1;i<10;i++) {
+        c[i] += c[i-1];
+    }
+    // c[i] now contains the number of elements less than or equal to i
+
+    for (int i=vi.size()-1;i>=0;i--) {
+        out[c[getdigit(vi[i],d)]-1] = vi[i];
+        c[getdigit(vi[i],d)]--;
+    }
+    vi = out;
+}
+
+void radixsort(vector<int> &vi)
+{
+    // first assume all int have same number of digits
+    int d = getnumofdigit(vi[0]);
+    for (int i=1;i<=d;i++) {
+        countingsort(vi,i);
+    }
+}
 
 static int partition(std::vector<int> &array, int low, int high)
 {
@@ -21,7 +75,7 @@ static int partition(std::vector<int> &array, int low, int high)
 static void merge(std::vector<int> &array, int start, int mid, int end)
 {
     int pos = start, q = mid+1, k = 0;
-    
+
     std::vector<int> temp;
     temp.reserve(end-start+1);
 
