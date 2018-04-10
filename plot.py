a4dfb491035cc66d6d727d6d405a37db2b1885f1
Lucas Hiroshi Horinouchi <lucas.hh@hotmail.com>
2018-04-09 23:11:17 -0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
 
-x = [10000, 50000, 100000, 500000, 1000000, 5000000, 10000000]
-x2 = ["10k", "50k", "100k", "500k", "1m", "5m", "10m"]
+x = [10, 100, 250, 500, 1000, 2500, 5000, 10000, 25000, 50000, 100000, 250000, 500000, 1000000, 2500000, 5000000, 10000000]
+x2 = ["10", "100", "250", "500", "1k", "2.5k", "5k", "10k", "25k", "50k", "100k", "250k", "500k", "1m", "2.5m", "5m", "10m"]
 
 
 def ssplit(l):
     length = len(l)
-    return [l[p:p + 7] for p in range(0, length, 7)]
+    return [l[p:p + 17] for p in range(0, length, 17)]
 
 
 def generate_results():
-    algs = ["quick", "merge"]
+    algs = ["quick", "merge", "radix"]
     res = []
 
     with open('data.txt') as file:
@@ -25,15 +25,22 @@ data = ssplit([float(x) for x in generate_results()])
 print(data)
 plt.figure(1)
 
-plt.plot(x2, data[0], 'ro')
+plt.plot(x2, data[0])
 plt.title('Quick Sort')
 plt.ylabel('Tempo(s)')
 plt.xlabel('Quantidade Numeros')
 plt.grid(True)
 plt.show()
 
-plt.plot(x2, data[1], 'ro')
+plt.plot(x2, data[1])
 plt.title('Merge Sort')
+plt.ylabel('Tempo(s)')
+plt.xlabel('Quantidade Numeros')
+plt.grid(True)
+plt.show()
+
+plt.plot(x2, data[2],)
+plt.title('Radix Sort')
 plt.ylabel('Tempo(s)')
 plt.xlabel('Quantidade Numeros')
 plt.grid(True)
